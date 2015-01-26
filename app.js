3e1ae20f9b7ae0fbba194e10a1781cc46c11513c
matiasalibertti <matias.alibertti@gmail.com>
2015-01-26 08:16:49 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// http://lostechies.com/seanbiefeld/2014/12/28/using-the-strategy-pattern-to-reduce-complexity-in-your-javascript/
+
+var UserRoleScheme = function (user) {
+
+    function _getUrl(subUrl) {
+
+        if (user.permissions.indexOf('write') > -1) {
+            return 'http://app/' + subUrl + '/admin';
+        } else {
+            return 'http://app/' + subUrl + '/index';
+        }
+
+    }
+
+    function _logUserRole() {
+        console.log('The current user is: ' + user.role);
+    }
+
+    this.tester = function () {
+        _logUserRole();
+        return _getUrl('testers');
+    };
+
+    this.developer = function () {
+        _logUserRole();
+        return _getUrl('developers');
+    };
+
+    this.manager = function () {
+        _logUserRole();
+        return _getUrl('managers');
+    };
+
+    this.default = function () {
+        console.warn('Error: The current user does not have a role');
+        return 'http://app/errors?c=403';
+    };
+
+};
+
+var user = {
+    role: 'developer',
+    permissions: ['execute', 'write']
+};
+
+var currentRoleHandler = new UserRoleScheme(user);
+
+console.log(currentRoleHandler);
+
+if (currentRoleHandler[user.role]) {
+    console.log(currentRoleHandler[user.role]());
+} else {
+    console.log(currentRoleHandler['default']());
+}
